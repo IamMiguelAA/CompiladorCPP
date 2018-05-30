fa4fa01c4fbfe8f055364d618dd6a035097f0c38
Miguel Ángel Álvarez <miguelangel.alvarezgarcia@alum.uca.es>
2018-05-30 21:36:03 +0200

    Programa de muestra

---
+++ program.c
@@ -0,0 +1,14 @@
+int fact(int n){ 
+if(n <= 1) 
+return 1; 
+else
+return n * fact(n-1); 
+}
+
+int numero; 
+int main(){
+printf("Dame un entero : ");
+scanf("%d", &numero);
+printf("El factorial = %d", fact(numero));
+return 0; 
+} 
